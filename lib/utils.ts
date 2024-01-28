c42a130e6c0c1f8ddb8cc12a00d2879b83d22ddf
jephthah <jephthah.mbah@outlook.com>
2024-01-28 20:29:57 +0100

    added NavigationProgressbar

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,3 +10,10 @@ export function isBase64Image(imageData: string) {
   const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
   return base64Regex.test(imageData);
 }
+
+export const errors = {
+  add: (error: unknown) => {
+    // link to sentry here
+    console.error(error);
+  },
+};
