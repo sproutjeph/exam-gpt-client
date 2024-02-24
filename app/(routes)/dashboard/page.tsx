f563d45d4b64f3c2f84dd47c69b57dce98f3b0d5
jephthah <jephthah.mbah@outlook.com>
2024-02-24 21:34:13 +0100

    pulled User name from kinde auth

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,14 +1,13 @@
-import { routes } from "@/constants/constants";
-import { Card } from "@/components/ui/card";
-import { ArrowRight } from "lucide-react";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import DashboardCard from "./_components/DashboardCard";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <main className="mx-4 my-4">
-      <h1 className="text-2xl">👋 Hi Good Morning Jephthah!</h1>
+      <h1 className="text-2xl">👋 Hi Good Morning {user?.given_name}!</h1>
       <p className="text-sm text-muted-foreground ml-8 mt-1 mb-8">
         Here's your summary for today
       </p>
@@ -30,40 +29,10 @@ const DashboardPage = () => {
         />
         <DashboardCard
           description="API calls"
-          imageUrl="/study.svg"
+          imageUrl="/mobile_testing.svg"
           value="0"
         />
       </div>
-
-      {/* <div className="my-8 space-y-4 ">
-        <h2 className="text-xl font-bold text-center sm:text-2xl md:text-4xl px-1">
-          Solve O-Level Past Questions with AI
-        </h2>
-        <p className="text-sm font-light text-center text-muted-foreground md:text-lg">
-          Learn with the smartest AI - Experience the power of AI
-        </p>
-      </div>
-      <div className="grid items-center justify-center grid-cols-2 gap-4 px-4 space-y-4 md:px-8 lg:px-32 2xl:grid-cols-3">
-        {routes.map((tool) => (
-          <Link href={tool.href} key={tool.href}>
-            <Card
-              className={cn(
-                `flex items-center justify-between p-4 transition  cursor-pointer border-black/5 hover:shadow-md`
-              )}
-            >
-              <div className="flex items-center gap-x-2">
-                <div className={cn("p-2 w-fit rounded-md")}>
-                  <tool.icon className={cn("w-8 h-8")} />
-                </div>
-                <div className="text-xs font-semibold sm:text-base">
-                  {tool.label}
-                </div>
-              </div>
-              <ArrowRight className="hidden w-5 h-5 sm:block" />
-            </Card>
-          </Link>
-        ))}
-      </div> */}
     </main>
   );
 };
